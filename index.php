05f53acdf0fba9c1794893ac13d7034cca483f29
Unknown <trunganh25397@gmail.com>
2018-02-01 16:21:30 +0200

    abc

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+class Model {
+	public $text;
+
+	public function __construct() {
+		$this->text = 'Hello world!';
+	}
+}
+
+class View {
+	private $model;
+
+	public function __construct(Model $model) {
+		$this->model = $model;
+	}
+
+	public function output() {
+		return '<a href="mvc.php?action=textclicked">' . $this->model->text . '</a>';
+	}
+
+}
+
+class Controller {
+	private $model;
+
+	public function __construct(Model $model) {
+		$this->model = $model;
+	}
+
+	public function textClicked() {
+		$this->model->text = 'Updated Hello World!';
+	}
+}
+
+
+$model = new Model();
+//It is important that the controller and the view share the model
+$controller = new Controller($model);
+$view = new View($model);
+if (isset($_GET['action'])) $controller->{$_GET['action']}();
+echo $view->output();
+?>
