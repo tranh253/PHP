df5cbb6667d94fbc8efa93eb8d3b2c2a68f115c4
Unknown <trunganh25397@gmail.com>
2018-02-01 20:11:27 +0200

    a

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ class Controller {
 
 
 $model = new Model();
-//It is important that the controller and the view share the model
 $controller = new Controller($model);
 $view = new View($model);
 if (isset($_GET['action'])) $controller->{$_GET['action']}();
